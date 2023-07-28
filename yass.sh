8f7861cb3938552f445c6498943a65e1f787d06c
Aaron Ogburn <aogburn@redhat.com>
2023-07-28 17:33:30 -0400

    [ISSUE 1] add access log summary

--- yass.sh
+++ yass.sh
@@ -327,21 +327,18 @@ if [ "$OPTIONS_SET" = "false" ] || [ "$SERVER" = "true" ]; then
 
     # highlight of yalas
     echo -e "${YELLOW}====== Server log yala summaries ======${NC}"
-    echo "====== Server log yala summaries ======" > $TARGET_DIR/server-log-summary.yass
-    echo "Number of server log files: $NUMBER_SERVER_LOGS" | tee -a $TARGET_DIR/server-log-summary.yass
-    echo | tee -a $TARGET_DIR/server-log-summary.yass
+    echo "====== Server log yala summaries ======" > $TARGET_DIR/server-log.yass-report
+    echo "Number of server log files: $NUMBER_SERVER_LOGS" | tee -a $TARGET_DIR/server-log.yass-report
+    echo | tee -a $TARGET_DIR/server-log.yass-report
     if [ $NUMBER_SERVER_LOGS -gt 0 ]; then
         for file in `find $TARGET_DIR -type f -iname \*.yala`; do
-            #for file in `find $TARGET_DIR -type f -iname \*.yala-summary`; do
-                #cat $file | tee -a $TARGET_DIR/server-log-summary.yass
-                #rm -rf $file
                 echo -e "${YELLOW}## Yala highlights of $file ##${NC}"
-                echo "## Yala highlights of $file ##" >> $TARGET_DIR/server-log-summary.yass
-                grep -R "*** First and last timestamped lines of" -A 2 $file | tee -a $TARGET_DIR/server-log-summary.yass
+                echo "## Yala highlights of $file ##" >> $TARGET_DIR/server-log.yass-report
+                grep -R "*** First and last timestamped lines of" -A 2 $file | tee -a $TARGET_DIR/server-log.yass-report
                 if [ -f $file-errors ]; then 
-                    grep "known ERRORS found of " $file-errors | tee -a $TARGET_DIR/server-log-summary.yass
+                    grep "known ERRORS found of " $file-errors | tee -a $TARGET_DIR/server-log.yass-report
                 fi
-        echo | tee -a $TARGET_DIR/server-log-summary.yass
+        echo | tee -a $TARGET_DIR/server-log.yass-report
         done
     fi
 fi 
@@ -356,8 +353,8 @@ if [ "$OPTIONS_SET" = "false" ] || [ "$THREAD" = "true" ]; then
 
     # highlight of yatdas
     echo -e "${YELLOW}====== Thread dump yatda summaries ======${NC}"
-    echo "====== Thread dump yatda summaries ======" > $TARGET_DIR/thread-dump-summary.yass
-    echo | tee -a $TARGET_DIR/thread-dump-summary.yass
+    echo "====== Thread dump yatda summaries ======" > $TARGET_DIR/thread-dump.yass-report
+    echo | tee -a $TARGET_DIR/thread-dump.yass-report
     if [ $NUMBER_THREAD_DUMPS -gt 0 ]; then
 
         MAX_NUMBER_THREADS=0
@@ -369,16 +366,16 @@ if [ "$OPTIONS_SET" = "false" ] || [ "$THREAD" = "true" ]; then
 
         for file in `find $TARGET_DIR -type f -iname \*.yatda`; do
             echo -e "${YELLOW}## Yatda summary of $file ##${NC}"
-            echo "## Yatda summary of $file ##" >> $TARGET_DIR/thread-dump-summary.yass
-            sed -n -e '/Number of thread dumps:/,/Specific findings/ p' $file | grep -v "Specific findings" | tee -a $TARGET_DIR/thread-dump-summary.yass
-            sed -n -e '/Top lines of request threads/,/Most common from first/ p' $file | grep -v "Most common from first" | tee -a $TARGET_DIR/thread-dump-summary.yass
+            echo "## Yatda summary of $file ##" >> $TARGET_DIR/thread-dump.yass-report
+            sed -n -e '/Number of thread dumps:/,/Specific findings/ p' $file | grep -v "Specific findings" | tee -a $TARGET_DIR/thread-dump.yass-report
+            sed -n -e '/Top lines of request threads/,/Most common from first/ p' $file | grep -v "Most common from first" | tee -a $TARGET_DIR/thread-dump.yass-report
             if [ -f $file-cpu ]; then 
-                grep "Max " -A 1 $file-cpu | tee -a $TARGET_DIR/thread-dump-summary.yass
+                grep "Max " -A 1 $file-cpu | tee -a $TARGET_DIR/thread-dump.yass-report
             fi
             if [ -f $file-gc-cpu ]; then 
-                grep "Max " -A 1 $file-gc-cpu | tee -a $TARGET_DIR/thread-dump-summary.yass
+                grep "Max " -A 1 $file-gc-cpu | tee -a $TARGET_DIR/thread-dump.yass-report
             fi
-            echo | tee -a $TARGET_DIR/thread-dump-summary.yass
+            echo | tee -a $TARGET_DIR/thread-dump.yass-report
 
             NUMBER_THREADS=`grep "Total number of threads:" $file | sed -E 's/^Total number of threads:  ([0-9]+)/\1/g'`
             if [ "$NUMBER_THREADS" != "" ]; then
@@ -455,7 +452,7 @@ $MAX_AVG_NUMBER_REQUESTS_FILE"
     fi
 
     echo -e "${YELLOW}====== Final thread dump summary ======${NC}"
-    echo "====== Final thread dump summary ======" >> $TARGET_DIR/thread-dump-summary.yass
+    echo "====== Final thread dump summary ======" >> $TARGET_DIR/thread-dump.yass-report
     {
         echo "Number of thread dump files: $NUMBER_THREAD_DUMPS"
         echo
@@ -479,18 +476,171 @@ $MAX_AVG_NUMBER_REQUESTS_FILE"
             echo "$MAX_AVG_NUMBER_REQUESTS_FILE"
             echo
         fi
-    }  | tee -a $TARGET_DIR/thread-dump-summary.yass
+    }  | tee -a $TARGET_DIR/thread-dump.yass-report
     echo -e "${YELLOW}====== Completed thread dump summary ======${NC}"
-    echo "====== Completed thread dump summary ======" >> $TARGET_DIR/thread-dump-summary.yass
+    echo "====== Completed thread dump summary ======" >> $TARGET_DIR/thread-dump.yass-report
 fi
 
 
 # Summarize access logs
 if [ "$OPTIONS_SET" = "false" ] || [ "$ACCESS" = "true" ]; then
-    echo -e "${GREEN}## Finding and summarizing access files ##${NC}"
+    echo -e "${GREEN}## Finding and summarizing access files in $TARGET_DIR  ##${NC}"
     echo
+    #peaks across all access logs
+    NUMBER_ACCESS_LOGS=0
+    PEAK_COMPLETED=0
+    PEAK_COMPLETED_DATE_FILE=0
+    PEAK_LONG=0
+    PEAK_LONG_DATE_FILE=0
+    PEAK_LONGEST=0
+    PEAK_LONGEST_DATE_FILE=0
+    PEAK_NUM4XXs=0
+    PEAK_NUM4XXs_DATE_FILE=0
+    PEAK_NUM5XXs=0
+    PEAK_NUM5XXs_DATE_FILE=0
+    for file in `find $TARGET_DIR -type f -iname \*access\*log\*`; do
+        if [[ ${file} != *".yass"* ]]; then
+            echo "    Summarizing $file" | tee $file-summary.yass-access
+           printf "%17s | %10s | %20s | %20s | %20s | %20s | %20s | %20s\n" "DATE" "COMPLETED" "LONGER(>10s)" "200s" "304s" "OTHER 3XXs" "4XXs" "5XXs" >> $file-summary.yass-access
+           echo "========================================================================================================================================================================" >> $file-summary.yass-access
 
-    # highlight of access logs
+            # completed - count of completed requests
+            # long - count of long requests (>10 sec)
+            TOTAL_COMPLETED=0
+            TOTAL_NUM200s=0
+            TOTAL_NUM304s=0
+            TOTAL_NUM3XXs=0
+            TOTAL_NUM4XXs=0
+            TOTAL_NUM5XXs=0
+            HIGHEST_COMPLETED=0
+            HIGHEST_COMPLETED_DATE=0
+            TOTAL_LONG=0
+            HIGHEST_LONG=0
+            HIGHEST_LONG_DATE=0
+            HIGHEST_NUM4XXs=0
+            HIGHEST_NUM4XXs_DATE=0
+            HIGHEST_NUM5XXs=0
+            HIGHEST_NUM5XXs_DATE=0
+
+            # highest longest request in this access log
+            HIGHEST_LONGEST=0
+
+            tmp=$file.yass-access-tmp
+
+            RESPONSE_TIMES=`grep -m 1 "[0-9]\.[0-9][0-9][0-9]$" $file | wc -l`
+            if [ $RESPONSE_TIMES -eq "0" ]; then
+                echo "        Response times not found in $file.  Use %T as last field of access-log pattern for response time summaries."
+            fi
+
+            DATES=`cat $file | awk '{ print $4 }' | sed 's/\[//'| sed 's/:[0-9][0-9]$//' | uniq`;
+            for x in $DATES; do
+                # longest this minute
+                LONGEST=0
+
+                grep "$x" $file > $tmp
+                COMPLETED=`cat $tmp | wc -l`
+                TOTAL_COMPLETED=$((TOTAL_COMPLETED + COMPLETED))
+                if [ $COMPLETED -gt $HIGHEST_COMPLETED ]; then
+                    HIGHEST_COMPLETED=$COMPLETED
+                    HIGHEST_COMPLETED_DATE=$x
+                    if [ $COMPLETED -gt $PEAK_COMPLETED ]; then
+                        PEAK_COMPLETED=$COMPLETED
+                        PEAK_COMPLETED_DATE_FILE="$x in $file"
+                    fi
+                fi
+
+
+                NUM200s=`grep -E "HTTP/[0-9]\.[0-9]\"? 200 " $tmp | wc -l`
+                TOTAL_NUM200s=$((TOTAL_NUM200s + NUM200s))
+
+                NUM304s=`grep -E "HTTP/[0-9]\.[0-9]\"? 200 " $tmp | wc -l`
+                TOTAL_NUM304s=$((TOTAL_NUM304s + NUM304s))
+
+                NUM3XXs=`grep -E "HTTP/[0-9]\.[0-9]\"? 3[0-9]([0-3]|[5-9]) " $tmp | wc -l`
+                TOTAL_NUM3XXs=$((TOTAL_NUM3XXs + NUM3XXs))
+
+                NUM4XXs=`grep -E "HTTP/[0-9]\.[0-9]\"? 4[0-9][0-9] " $tmp | wc -l`
+                TOTAL_NUM4XXs=$((TOTAL_NUM4XXs + NUM4XXs))
+                if [ $NUM4XXs -gt $HIGHEST_NUM4XXs ]; then
+                    HIGHEST_NUM4XXs=$NUM4XXs
+                    HIGHEST_NUM4XXs_DATE=$x
+                    if [ $NUM4XXs -gt $PEAK_NUM4XXs ]; then
+                        PEAK_NUM4XXs=$NUM4XXs
+                        PEAK_NUM4XXs_DATE_FILE="$x in $file"
+                    fi
+                fi
+
+                NUM5XXs=`grep -E "HTTP/[0-9]\.[0-9]\"? 5[0-9][0-9] " $tmp | wc -l`
+                TOTAL_NUM5XXs=$((TOTAL_NUM5XXs + NUM5XXs))
+                if [ $NUM5XXs -gt $HIGHEST_NUM5XXs ]; then
+                    HIGHEST_NUM5XXs=$NUM5XXs
+                    HIGHEST_NUM5XXs_DATE=$x
+                    if [ $NUM5XXs -gt $PEAK_NUM5XXs ]; then
+                        PEAK_NUM5XXs=$NUM5XXs
+                        PEAK_NUM5XXs_DATE_FILE="$x in $file"
+                    fi
+                fi
+
+
+                if [ $RESPONSE_TIMES -ne "0" ]; then
+                    LONG=`grep -v " [0-9]\.[0-9][0-9][0-9]$" $tmp | wc -l`
+
+                    TOTAL_LONG=$((TOTAL_LONG + LONG))
+                    if [ $LONG -gt $HIGHEST_LONG ]; then
+                        HIGHEST_LONG=$LONG
+                        HIGHEST_LONG_DATE=$x
+                        if [ $LONG -gt $PEAK_LONG ]; then
+                            PEAK_LONG=$LONG
+                            PEAK_LONG_DATE_FILE="$x in $file"
+                        fi
+                    fi
+
+                    PERCENT_LONG=`printf %.2f $((10**4 * $LONG / $COMPLETED ))e-2`
+                    PERCENT_NUM200s=`printf %.2f $((10**4 * $NUM200s / $COMPLETED ))e-2`
+                    PERCENT_NUM304s=`printf %.2f $((10**4 * $NUM304s / $COMPLETED ))e-2`
+                    PERCENT_NUM3XXs=`printf %.2f $((10**4 * $NUM3XXs / $COMPLETED ))e-2`
+                    PERCENT_NUM4XXs=`printf %.2f $((10**4 * $NUM4XXs / $COMPLETED ))e-2`
+                    PERCENT_NUM5XXs=`printf %.2f $((10**4 * $NUM5XXs / $COMPLETED ))e-2`
+                    printf "%s | %10s | %10s - %6s%% | %10s - %6s%% | %10s - %6s%% | %10s - %6s%% | %10s - %6s%% | %10s - %6s%%\n" $x $COMPLETED $LONG $PERCENT_LONG $NUM200s $PERCENT_NUM200s $NUM304s $PERCENT_NUM304s $NUM3XXs $PERCENT_NUM3XXs $NUM4XXs $PERCENT_NUM4XXs $NUM5XXs $PERCENT_NUM5XXs >> $file-summary.yass-access
+                else
+                    printf "%s | Completed: %10s\n" $d $COMPLETED >> $file-summary.yass-access
+                fi
+            done
+
+            echo >> $file-summary.yass-access
+            echo -e "${YELLOW}## Access log summary of $file ##${NC}"
+            echo "## Access log summary of $file ##" >> $TARGET_DIR/access-log.yass-report
+            {
+                echo "* Number of requests: $TOTAL_COMPLETED"
+                echo "* Number of 200s: $TOTAL_NUM200s - `printf %.2f $((10**4 * $TOTAL_NUM200s / $TOTAL_COMPLETED ))e-2`%"
+                echo "* Number of 304s: $TOTAL_NUM304s - `printf %.2f $((10**4 * $TOTAL_NUM304s / $TOTAL_COMPLETED ))e-2`%"
+                echo "* Number of 4XXs: $TOTAL_NUM3XXs - `printf %.2f $((10**4 * $TOTAL_NUM3XXs / $TOTAL_COMPLETED ))e-2`%"
+                echo "* Number of 4XXs: $TOTAL_NUM4XXs - `printf %.2f $((10**4 * $TOTAL_NUM4XXs / $TOTAL_COMPLETED ))e-2`%"
+                echo "* Number of 5XXs: $TOTAL_NUM5XXs - `printf %.2f $((10**4 * $TOTAL_NUM5XXs / $TOTAL_COMPLETED ))e-2`%"
+                echo "* Highest completed request count is $HIGHEST_COMPLETED at $HIGHEST_COMPLETED_DATE"
+                echo "* Highest long response count is $HIGHEST_LONG at $HIGHEST_LONG_DATE"
+                echo "* Highest long response count is $HIGHEST_LONG at $HIGHEST_LONG_DATE"
+                echo "* Highest number of 4XX responses is $HIGHEST_NUM4XXs at $HIGHEST_NUM4XXs_DATE"
+                echo "* Highest number of 5XX responses is $HIGHEST_NUM5XXs at $HIGHEST_NUM5XXs_DATE"
+                echo
+            } | tee -a $TARGET_DIR/access-log.yass-report
+            rm -rf $tmp
+            NUMBER_ACCESS_LOGS=$((NUMBER_ACCESS_LOGS+1))
+        fi
+    done
+
+
+    echo -e "${YELLOW}====== Final access log summary ======${NC}"
+    echo "====== Final access log summary ======" >> $TARGET_DIR/access-log.yass-report
+    {
+        echo "Number of access log files: $NUMBER_ACCESS_LOGS" | tee -a $TARGET_DIR/access-log.yass-report
+        echo "* Peak completed request count is $PEAK_COMPLETED at $PEAK_COMPLETED_DATE_FILE"
+        echo "* Peak long response count is $PEAK_LONG at $PEAK_LONG_DATE_FILE"
+        echo "* Peak number of 4XX responses is $PEAK_NUM4XXs at $PEAK_NUM4XXs_DATE_FILE"
+        echo "* Peak number of 5XX responses is $PEAK_NUM5XXs at $PEAK_NUM5XXs_DATE_FILE"
+    } | tee -a $TARGET_DIR/access-log.yass-report
+    echo -e "${YELLOW}====== Completed access log summary ======${NC}"
+    echo "====== Completed access log summary ======" >> $TARGET_DIR/access-log.yass-report
 fi
 
 
@@ -505,8 +655,8 @@ if [ "$OPTIONS_SET" = "false" ] || [ "$GC" = "true" ]; then
 
     # highlight of GC logs
     echo -e "${YELLOW}====== Garbagecat GC log summaries ======${NC}"
-    echo "====== Garbagecat GC log summaries ======" > $TARGET_DIR/gc-log-summary.yass
-    echo | tee -a $TARGET_DIR/gc-log-summary.yass
+    echo "====== Garbagecat GC log summaries ======" > $TARGET_DIR/gc-log.yass-report
+    echo | tee -a $TARGET_DIR/gc-log.yass-report
 
     if [ $NUMBER_GC_LOGS -gt 0 ]; then
         LOWEST_THROUGHPUT=100
@@ -515,9 +665,9 @@ if [ "$OPTIONS_SET" = "false" ] || [ "$GC" = "true" ]; then
 
         for file in `find $TARGET_DIR -type f -iname \*.garbagecat-report`; do
             echo -e "${YELLOW}## Garbagecat GC log summary of $file ##${NC}"
-            echo "## Garbagecat GC log summary of $file ##" >> $TARGET_DIR/gc-log-summary.yass
-            sed -n -E -e '/(JVM|SUMMARY):/,/ANALYSIS:/ p' $file | grep -v "ANALYSIS:" | tee -a $TARGET_DIR/gc-log-summary.yass
-            echo | tee -a $TARGET_DIR/gc-log-summary.yass
+            echo "## Garbagecat GC log summary of $file ##" >> $TARGET_DIR/gc-log.yass-report
+            sed -n -E -e '/(JVM|SUMMARY):/,/ANALYSIS:/ p' $file | grep -v "ANALYSIS:" | tee -a $TARGET_DIR/gc-log.yass-report
+            echo | tee -a $TARGET_DIR/gc-log.yass-report
 
             # Track lowest throughput to report
             THROUGHPUT=`grep "GC Throughput" $file | sed -E 's/^[^0-9]+([0-9]+)[^0-9]+/\1/g'`
@@ -548,17 +698,17 @@ $file"
 
 
     echo -e "${YELLOW}====== Final GC summary ======${NC}"
-    echo "====== Final GC summary ======" >> $TARGET_DIR/gc-log-summary.yass
+    echo "====== Final GC summary ======" >> $TARGET_DIR/gc-log.yass-report
     # Output low & max
     {
-        echo "Number of GC log files: $NUMBER_GC_LOGS" | tee -a $TARGET_DIR/gc-log-summary.yass
+        echo "Number of GC log files: $NUMBER_GC_LOGS" | tee -a $TARGET_DIR/gc-log.yass-report
         if [ $NUMBER_GC_LOGS -gt 1 ]; then
             echo "* Lowest throughput is $LOWEST_THROUGHPUT in files:"
             echo "$LOWEST_THROUGHPUT_FILE"
             echo
             echo "* Max pause is $MAX_PAUSE_SECONDS.$MAX_PAUSE_MILLIS in $MAX_PAUSE_FILE"
         fi
-    }  | tee -a $TARGET_DIR/gc-log-summary.yass
+    }  | tee -a $TARGET_DIR/gc-log.yass-report
     echo -e "${YELLOW}====== Completed GC summary ======${NC}"
-    echo "====== Completed GC summary ======" >> $TARGET_DIR/gc-log-summary.yass
+    echo "====== Completed GC summary ======" >> $TARGET_DIR/gc-log.yass-report
 fi 
